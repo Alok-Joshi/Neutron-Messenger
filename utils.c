3262b7720dbd1195d2f530f46fa47ecc8edaff6b
Alok Joshi <AlokJoshi523@gmail.com>
2022-08-27 14:05:22 +0530

    Implemented basic json serializer

--- utils.c
+++ utils.c
@@ -1,5 +1,59 @@
 #include "utils.h"
 
+char * get_key_value_string(char* key, char* value){
+		/* Converts a key value into string key:value */
+
+		char * key_val = (char*)malloc(1000);
+		strcpy(key_val,""); //clears the string
+
+		strcat(key_val,"\"");
+		strcat(key_val,key);
+		strcat(key_val,"\"");
+		strcat(key_val," : ");
+
+		strcat(key_val, "\"");
+		strcat(key_val,value);
+		strcat(key_val, "\"");
+
+		return key_val;
+		
+
+}
+int get_size(char** keys, char** values, int size){
+	
+	int json_size = 0;
+	for(int i = 0; i<size; i++){
+		json_size += sizeof(keys[i]) + sizeof(values[i]);
+	}
+	return json_size;
+}
+
+char* json_stringify(char** keys, char **values,int size){
+	
+	int json_size = get_size(keys,values,size);
+
+	char* json_string = (char*)malloc(4*json_size);
+	strcat(json_string,"{");
+
+	for(int i = 0; i<size; ++i){
+
+		char * key_val = get_key_value_string(keys[i],values[i]);
+		strcat(json_string,key_val);
+		free(key_val);
+
+		if( i != size-1){
+			strcat(json_string,",");
+		}
+	}
+	strcat(json_string,"}");
+	return json_string;
+
+}
+
+
+/* PASSWORD RELATED UTILITY FUNCTIONS */
+/*===============================================================================*/
+
 int passisempty(char *pass)  //returns true if password is empty
 {
    for(int i = 0; i<strlen(pass); i++)
@@ -62,6 +116,52 @@ int isunique(char *name)
    }
     pthread_mutex_unlock(&mutex);  
     return 1;
+}
+
+int iscensored(char *word)
+{
+  for(int i = 0; i<censored_word_count; i++)
+  {
+     if(strcmp(word,censored_words[i]) == 0) return 1;
+     
+  }
+  return 0;
+}
+
+char* censor(char *message)    
+{
+  char *new_message = (char*)calloc(1000,sizeof(char));
+  strcpy(new_message,"");
+  char temp[10000];
+  
+  for(int i = 0,j=0; i<=strlen(message); i++)
+  {
+     if(message[i] == ' '|| message[i]== '\0'|| message[i] == '?' || message[i] == ',')
+     { 
+       
+        temp[j] = '\0';
+        j = 0;
+       
+        if(iscensored(temp))
+        {
+           strcat(new_message, "*CENSORED* ");
+           
+        }
+        else
+        {
+          strcat(new_message, temp);
+          strcat(new_message, " ");
+      
+        }
+        strcpy(temp,"");
+     }
+     else
+       {
+        temp[j++] = message[i];
+       }
+  }
+  
+  return new_message;
 }
 
 
--- utils.h
+++ utils.h
@@ -1,9 +1,16 @@
 
 int passisempty(char *pass);
+
 void set_password();
+
 void set_censored_words();
 
 int isunique(char *name);
+
 int iscensored(char *word);
+
 char* censor(char *message);    
 
+int get_size(char** keys, char** values, int size);
+
+char * json_stringify_single(char*keys,char* values, int size);
